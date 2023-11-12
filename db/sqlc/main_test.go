1d69fe2e925c464129982d36f113036751e9f32e
khafizullokh02 <khafizullokhulmasov@gmail.com>
2023-11-12 22:28:44 +0500

    store_test.go

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -9,22 +9,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var testQueries *Queries
-
-// var testStore Store
-
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
 )
 
-func TestMain(m *testing.M ) {
-	conn, err := sql.Open(dbDriver, dbSource)
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
