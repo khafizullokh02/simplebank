d4a2b1a3324b8a3fb12b84ad32c75660cbad2edb
khafizullokh02 <khafizullokhulmasov@gmail.com>
2023-12-09 11:49:42 +0500

    paseto_maker_test.go

--- token/paseto_maker_test.go
+++ token/paseto_maker_test.go
@@ -1,51 +1,66 @@
 package token
 
-// import (
-// 	"testing"
-// 	"time"
+import (
+	"testing"
+	"time"
 
-// 	"github.com/khafizullokh02/simplebank/util"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/khafizullokh02/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestPasetoMaker(t *testing.T) {
-// 	maker, err := NewPasetoMaker(util.RandomString(32))
-// 	require.NoError(t, err)
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
 
-// 	username := util.RandomOwner()
-// 	role := util.DepositorRole
-// 	duration := time.Minute
+	username := util.RandomOwner()
+	duration := time.Minute
 
-// 	issuedAt := time.Now()
-// 	expiredAt := issuedAt.Add(duration)
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
 
-// 	token, payload, err := maker.CreateToken(username, role, duration)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, token)
-// 	require.NotEmpty(t, payload)
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
 
-// 	payload, err = maker.VerifyToken(token)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, token)
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
 
-// 	require.NotZero(t, payload.ID)
-// 	require.Equal(t, username, payload.Username)
-// 	require.Equal(t, role, payload.Role)
-// 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
-// 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
-// }
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
 
-// func TestExpiredPasetoToken(t *testing.T) {
-// 	maker, err := NewPasetoMaker(util.RandomString(32))
-// 	require.NoError(t, err)
+func TestInvalidPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
 
-// 	token, payload, err := maker.CreateToken(util.RandomOwner(), util.DepositorRole, -time.Minute)
-// 	require.NoError(t, err)
-// 	require.NotEmpty(t, token)
-// 	require.NotEmpty(t, payload)
+	username := util.RandomOwner()
+	duration := time.Minute
 
-// 	payload, err = maker.VerifyToken(token)
-// 	require.Error(t, err)
-// 	require.EqualError(t, err, ErrExpiredToken.Error())
-// 	require.Nil(t, payload)
-// }
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+
+	payload, err := NewPayload(token, duration)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
